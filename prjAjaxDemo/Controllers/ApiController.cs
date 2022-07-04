37533e234404ebce658ccf2c7134318e155fd3f3
DeanHao <zeroqazggc0504@gmail.com>
2022-07-04 10:27:19 +0800

    如何回傳 content 及設定content type,設定編碼

---
+++ prjAjaxDemo/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjAjaxDemo.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("<h1>Ajax 你好</h1>","text/html",System.Text.Encoding.UTF8);
+        }
+    }
+}
