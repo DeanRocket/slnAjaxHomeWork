a1cd1acd73f9ec0f93e4478619061fbc51554ced
DeanHao <zeroqazggc0504@gmail.com>
2022-07-06 13:17:02 +0800

    HomeWork2 驗證姓名是否重複 Controller:Home Action:Create

---
+++ prjHomeWork2/Controllers/ApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjHomeWork2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjHomeWork2.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _demoContext;
+        public ApiController( DemoContext demoContext)
+        {
+           
+            _demoContext = demoContext;
+        }
+
+        public IActionResult CheckName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+                return Json("請填入姓名");
+
+            var q = _demoContext.Members.Where(c => c.Name == Name).FirstOrDefault();
+            if (q == null)
+                return Json("可以使用此名稱");
+
+            return Json("此ID已有人使用");
+            
+        }
+    }
+}
